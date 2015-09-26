a367d0379ba5bd0b960061162c4f50e8388d7e69
Solaris Nite <solarisnite@gmail.com>
2015-09-26 16:01:36 -0500

    commit

--- up/FirstViewController.swift
+++ up/FirstViewController.swift
@@ -6,19 +6,26 @@
 //  Copyright © 2015 StudyUp. All rights reserved.
 //
 
-import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
 
 class FirstViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        
+        
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+
 
 
 }
